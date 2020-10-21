b0fe8adec7a05300e442d216e32bc0c95f81c177
Radu <radu.ciucanu@insa-cvl.fr>
2020-10-21 15:19:05 +0200

    experiments wip

--- script_SR_SP.py
+++ script_SR_SP.py
@@ -7,8 +7,7 @@ from tools import parse_json_output, check_results
 import warnings
 warnings.simplefilter("ignore")
 
-
-nb_runs = 1
+nb_runs = 30
 # set_random != 0 means that random seed will be chosen classicaly, using time
 # set_random == 0 means that random seed is fixed to 1 each time an algo is executed nb_runs times
 set_random = 1
@@ -37,37 +36,31 @@ for K in K_vals:
                 for algo in algos:
                         print ("*" * 10 + " K=", K, "N=", N, "algo=", algo)
                         output_file = DIR + "K_" + str(K) + "_N_" + str(N) + "_" + algo + ".txt"
-                        #os.system("python3 " + algo + ".py " + str(nb_runs) + " " + str(N) + " " + input_file + " " + output_file + " " + str(set_random))
+                        os.system("python3 " + algo + ".py " + str(nb_runs) + " " + str(N) + " " + input_file + " " + output_file + " " + str(set_random))
                         R_list[algo], aggregate_time, aggregates_all[algo] = parse_json_output(output_file)
                         aggregates_time[algo + "_" + str(K)].append(aggregate_time)
 
 # generate plot
-
 algos=[]
 algos_names=[]
-
 for K in K_vals:
   algos.append("SR_Secure_ISPEC_" + str(K))
   algos.append("SR_SP_" + str(K))
   algos_names.append("K=" + str(K) + ", SR-Paillier-Old")
   algos_names.append("K=" + str(K) + ", SR-Paillier")
 
-
+# plot one line for each algorithm
 plt.figure(figsize=(8, 4))
 plt.rcParams.update({'font.size':14})
-
-# plot one line for each algorithm
 ax = plt.subplot(121)
 markers = ('x', 'o', '*', '.', '', '', '')
 for algo in algos:
         plt.plot(N_vals, aggregates_time[algo], marker=markers[algos.index(algo)], fillstyle='none', markersize=10)
 plt.legend(algos_names, bbox_to_anchor=(2.25, 0.75))
-
 plt.xticks(N_vals, N_vals)
 plt.xlabel("Budget N")
 plt.ylabel('Time (seconds)')
 plt.subplots_adjust(top=0.92, bottom=0.2)
-
 plt.savefig(DIR + "plot_lines_SR_P" + ".pdf")
 plt.clf()
 
